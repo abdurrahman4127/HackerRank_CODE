a5d0b6482b2610c83b2f6ae4d8344380860672e2
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-04-10 18:50:23 +0000

    Add files via upload

---
+++ MinMaxSum/src/MinMAx.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMAx {
+    public static void main(String[] args)
+    {
+        int []A = new int[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(A);
+
+        long minSum = 0, maxSum = 0;
+        for (int i = 0; i < 5-1; i++) {
+            minSum += A[i];
+        }
+
+        for (int i = 1; i < 5; i++) {
+            maxSum += A[i];
+        }
+
+        System.out.println(minSum + " " + maxSum);
+    }
+}
