b35aeb4f8a97cfc37f10131b47088732cca436c9
Abdur Rahman <71093608+abdurrahman4127@users.noreply.github.com>
2022-10-29 12:25:40 +0600

    Update AngryProfessor.cpp

--- Angry Professor/AngryProfessor.cpp
+++ Angry Professor/AngryProfessor.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int a; cin >> a;
-    for (int g=0; g<a; g++)
-    {
-        int b,c; cin >> b >> c;
+    
+    for (int g=0; g<a; g++) {
+        int b, c; cin >> b >> c;
+        
         int num=0; 
-        for (int g=0; g<b; g++)
-        {
+        for (int g=0; g<b; g++) {
             int d; cin >> d;
-            if (d<=0) num++; 
+            if (d <= 0) 
+                num++; 
         }
-        if (num>=c)
-        {
+        
+        if (num >= c)
             cout << "NO" << '\n'; 
-        }
-        else cout << "YES" << '\n';
+        else 
+            cout << "YES" << '\n';
     }
+    
     return 0; 
 }
